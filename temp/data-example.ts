8d0b20421cdf345a1b31be3bdf0218a9857c08b2
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2023-08-13 22:42:27 +0800

    add svg

---
+++ temp/data-example.ts
@@ -0,0 +1,291 @@
+const dataExample =  {
+  "loc": 9255,
+  "locByLangs": {
+  ".go": 8060,
+    ".md": 782,
+    ".json": 260,
+    ".yml": 47,
+    ".pem": 46,
+    "Makefile": 17,
+    "LICENSE": 16,
+    ".mod": 9,
+    ".sum": 8,
+    ".svg": 6,
+    ".gitignore": 3,
+    ".txt": 1
+},
+  "children": {
+  "middleware": {
+    "loc": 3254,
+      "locByLangs": {
+      ".go": 3254
+    },
+    "children": {
+      "compress.go": 355,
+        "strip_test.go": 202,
+        "compress_test.go": 196,
+        "throttle_test.go": 190,
+        "wrap_writer.go": 184,
+        "recoverer.go": 176,
+        "logger.go": 146,
+        "route_headers.go": 146,
+        "middleware_test.go": 134,
+        "throttle.go": 111,
+        "content_charset_test.go": 108,
+        "realip_test.go": 91,
+        "request_id.go": 84,
+        "content_encoding_test.go": 73,
+        "content_type_test.go": 72,
+        "url_format.go": 65,
+        "get_head_test.go": 59,
+        "strip.go": 58,
+        "request_id_test.go": 56,
+        "terminal.go": 56,
+        "url_format_test.go": 56,
+        "profiler.go": 54,
+        "realip.go": 53,
+        "recoverer_test.go": 52,
+        "nocache.go": 48,
+        "timeout.go": 46,
+        "content_charset.go": 43,
+        "content_type.go": 42,
+        "logger_test.go": 41,
+        "get_head.go": 33,
+        "content_encoding.go": 32,
+        "basic_auth.go": 28,
+        "heartbeat.go": 24,
+        "clean_path.go": 23,
+        "middleware.go": 19,
+        "wrap_writer_test.go": 19,
+        "page_route.go": 18,
+        "maybe.go": 16,
+        "request_size.go": 16,
+        "value.go": 15,
+        "path_rewrite.go": 14
+    }
+  },
+  "_examples": {
+    "loc": 1653,
+      "locByLangs": {
+      ".go": 1217,
+        ".json": 260,
+        ".md": 154,
+        ".sum": 8,
+        ".mod": 7,
+        ".svg": 6,
+        ".txt": 1
+    },
+    "children": {
+      "rest": {
+        "loc": 855,
+          "locByLangs": {
+          ".go": 446,
+            ".json": 260,
+            ".md": 134,
+            ".sum": 8,
+            ".mod": 7
+        },
+        "children": {
+          "main.go": 446,
+            "routes.json": 260,
+            "routes.md": 134,
+            "go.sum": 8,
+            "go.mod": 7
+        }
+      },
+      "versions": {
+        "loc": 236,
+          "locByLangs": {
+          ".go": 236
+        },
+        "children": {
+          "main.go": 136,
+            "presenter": {
+            "loc": 68,
+              "locByLangs": {
+              ".go": 68
+            },
+            "children": {
+              "v3": {
+                "loc": 30,
+                  "locByLangs": {
+                  ".go": 30
+                },
+                "children": {
+                  "article.go": 30
+                }
+              },
+              "v2": {
+                "loc": 22,
+                  "locByLangs": {
+                  ".go": 22
+                },
+                "children": {
+                  "article.go": 22
+                }
+              },
+              "v1": {
+                "loc": 16,
+                  "locByLangs": {
+                  ".go": 16
+                },
+                "children": {
+                  "article.go": 16
+                }
+              }
+            }
+          },
+          "data": {
+            "loc": 32,
+              "locByLangs": {
+              ".go": 32
+            },
+            "children": {
+              "errors.go": 24,
+                "article.go": 8
+            }
+          }
+        }
+      },
+      "logging": {
+        "loc": 120,
+          "locByLangs": {
+          ".go": 120
+        },
+        "children": {
+          "main.go": 120
+        }
+      },
+      "todos-resource": {
+        "loc": 103,
+          "locByLangs": {
+          ".go": 103
+        },
+        "children": {
+          "todos.go": 40,
+            "users.go": 36,
+            "main.go": 27
+        }
+      },
+      "limits": {
+        "loc": 79,
+          "locByLangs": {
+          ".go": 79
+        },
+        "children": {
+          "main.go": 79
+        }
+      },
+      "graceful": {
+        "loc": 66,
+          "locByLangs": {
+          ".go": 66
+        },
+        "children": {
+          "main.go": 66
+        }
+      },
+      "fileserver": {
+        "loc": 62,
+          "locByLangs": {
+          ".go": 61,
+            ".txt": 1
+        },
+        "children": {
+          "main.go": 61,
+            "data": {
+            "loc": 1,
+              "locByLangs": {
+              ".txt": 1
+            },
+            "children": {
+              "notes.txt": 1
+            }
+          }
+        }
+      },
+      "router-walk": {
+        "loc": 34,
+          "locByLangs": {
+          ".go": 34
+        },
+        "children": {
+          "main.go": 34
+        }
+      },
+      "custom-method": {
+        "loc": 29,
+          "locByLangs": {
+          ".go": 29
+        },
+        "children": {
+          "main.go": 29
+        }
+      },
+      "custom-handler": {
+        "loc": 27,
+          "locByLangs": {
+          ".go": 27
+        },
+        "children": {
+          "main.go": 27
+        }
+      },
+      "README.md": 20,
+        "hello-world": {
+        "loc": 16,
+          "locByLangs": {
+          ".go": 16
+        },
+        "children": {
+          "main.go": 16
+        }
+      },
+      "chi.svg": 6
+    }
+  },
+  "mux_test.go": 1612,
+    "tree.go": 749,
+    "tree_test.go": 443,
+    "mux.go": 423,
+    "README.md": 393,
+    "CHANGELOG.md": 211,
+    "context.go": 137,
+    "chi.go": 117,
+    "context_test.go": 68,
+    ".github": {
+    "loc": 47,
+      "locByLangs": {
+      ".yml": 47
+    },
+    "children": {
+      "workflows": {
+        "loc": 36,
+          "locByLangs": {
+          ".yml": 36
+        },
+        "children": {
+          "ci.yml": 36
+        }
+      },
+      "FUNDING.yml": 11
+    }
+  },
+  "testdata": {
+    "loc": 46,
+      "locByLangs": {
+      ".pem": 46
+    },
+    "children": {
+      "key.pem": 27,
+        "cert.pem": 19
+    }
+  },
+  "chain.go": 40,
+    "CONTRIBUTING.md": 24,
+    "Makefile": 17,
+    "LICENSE": 16,
+    ".gitignore": 3,
+    "go.mod": 2
+}
+}
